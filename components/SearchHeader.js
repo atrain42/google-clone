91ff0275e332a553fa04702cb29e3ad5255fb553
atrain42 <maxheimer4211@gmail.com>
2022-09-22 16:25:44 -0400

    site updates

---
+++ components/SearchHeader.js
@@ -0,0 +1,57 @@
+import React, { useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+
+//components
+import User from "../components/User";
+
+//icons
+import { FiSearch } from "react-icons/fi";
+import { HiMicrophone } from "react-icons/hi";
+import { BsXLg } from "react-icons/bs";
+
+export default function SearchHeader() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+
+    const term = searchInputRef.current.value;
+
+    if (!term.trim()) return;
+
+    router.push(`/search?term=${term.trim()}`);
+  };
+
+  return (
+    <header className="sticky top-0 bg-white">
+      <div className="flex w-full p-6 items-center">
+        <Image
+          onClick={() => router.push("/")}
+          width="120"
+          objectFit="contain"
+          height="40"
+          className="cursor-pointer"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png"
+        />
+        <form className="flex border border-gray-200 rounded-full shadow-md px-5 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center">
+          <input
+            className="w-full focus:outline-none"
+            type="text"
+            defaultValue={router.query.term}
+            ref={searchInputRef}
+          />
+          <BsXLg
+            onClick={() => (searchInputRef.current.value = "")}
+            className="h-7 text-gray-400 cursor-pointer sm:mr-3"
+          />
+          <HiMicrophone className="h-7 w-8 hidden sm:inline-flex text-blue-500 pl-4 border-l-2 border-gray-300 pl-3" />
+          <FiSearch className="h-7 w-5 hidden sm:inline-flex text-blue-500 ml-2" />
+          <button onClick={searchHandler} type="submit" hidden></button>
+        </form>
+        <User className="ml-auto whitespace-nowrap" />
+      </div>
+    </header>
+  );
+}
---
+++ pages/search.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+import SearchHeader from "../components/SearchHeader";
+
+export default function Search() {
+  return (
+    <>
+      <Head>
+        <title>Search page</title>
+      </Head>
+      <SearchHeader />
+    </>
+  );
+}
