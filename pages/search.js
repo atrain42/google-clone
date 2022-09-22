1664364c4b382829ad1efaa0963142a8370fee88
atrain42 <maxheimer4211@gmail.com>
2022-09-22 18:32:53 -0400

    site updates

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
+import Response from "../Response";
 
 import SearchHeader from "../components/SearchHeader";
 
-export default function Search() {
+export default function Search({ res }) {
+  console.log(res);
   return (
     <>
       <Head>
@@ -11,4 +13,22 @@ export default function Search() {
       <SearchHeader />
     </>
   );
+}
+
+export async function getServerSideProps(context) {
+  const mockData = true;
+  const data = mockData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${
+          process.env.API_KEY
+        }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+          context.query.searchType && "&searchType=image"
+        }`
+      ).then((res) => res.json());
+  return {
+    props: {
+      res: data,
+    },
+  };
 }
