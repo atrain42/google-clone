ac39571ce6b18d12c5385e550e113541a37cc6e9
atrain42 <maxheimer4211@gmail.com>
2022-09-22 13:54:23 -0400

    git the input ref and redirect to related page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,21 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { FiSearch } from "react-icons/fi";
 import { HiMicrophone } from "react-icons/hi";
+import { useRouter } from "next/router";
+import { useRef } from "react";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    const term = searchInputRef.current.value;
+
+    if (!term.trim()) return;
+    router.push(`/search?term${term.trim()}`);
+  };
+
   return (
     <div>
       <Head>
@@ -23,11 +36,17 @@ export default function Home() {
         />
         <div className="flex w-full mt-5 mx-auto max-w-[90%] border border-gray-200 items-center hover:shadow-lg focus-within:shadow-lg px-5 py-3 rounded-full sm:max-w-xl lg:max-w-2xl">
           <FiSearch className="h-5 text-gray-500 mr-3" />
-          <input type="text" className="flex-grow focus:outline-none"></input>
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none"
+          ></input>
           <HiMicrophone />
         </div>
         <div className="flex flex-col sm:flex-row w-[50%] space-y-2 mt-8 sm:space-y-0 sm:space-x-4 justify-center">
-          <button className="btn">Google Search</button>
+          <button onClick={searchHandler} className="btn">
+            Google Search
+          </button>
           <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
