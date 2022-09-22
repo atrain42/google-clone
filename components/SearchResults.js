f7817a0fcfd81df1247a6e2f2382644b9316428d
atrain42 <maxheimer4211@gmail.com>
2022-09-22 19:31:44 -0400

    added search results completion

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+//convert hmtl string
+import Parser from "html-react-parser";
+
 export default function SearchResults({ res }) {
   return (
     <div className="w-full mx-auto px-3 sm:pl-[5%] md:pl-[14%] lg:pl-52">
@@ -7,6 +10,24 @@ export default function SearchResults({ res }) {
         About {res.searchInformation.formattedTotalResults} results (
         {res.searchInformation.formattedSearchTime} seconds)
       </p>
+      {res.items.map((result, i) => (
+        <div key={i} className="max-w-xl mb-8">
+          <div className="group">
+            <a className="text-sm truncate" href={result.link}>
+              {result.formattedUrl}
+            </a>
+            <a
+              className="group-hover:underline decoration-blue-800"
+              href={result.link}
+            >
+              <h2 className="truncate text-xl font-medium text-blue-800">
+                {result.title}
+              </h2>
+            </a>
+          </div>
+          <p>{Parser(result.htmlSnippet)}</p>
+        </div>
+      ))}
     </div>
   );
 }
