2e246655d3750e35c403482cdff4587d8b50cb91
atrain42 <maxheimer4211@gmail.com>
2022-09-23 11:17:27 -0400

    site updates

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="absolute bottom-[0%] left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
+    <footer className="flex flex-col items-center absolute bottom-[0%] left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
+      <p>For learning purposes</p>
       <p>Copyright &copy; {new Date().getFullYear()} Austin Maxheimer</p>
     </footer>
   );
